3b89243e04d36563f31046b75cf985b523910076
functional-puppet-3692 <samadshaik@live.com>
2018-10-30 21:21:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
 
   if (string.toLowerCase()) {
     console.log(string.toLowerCase());
-    return string.toLowerCase();
+    //return string.toLowerCase();
   }
 }
 // //
