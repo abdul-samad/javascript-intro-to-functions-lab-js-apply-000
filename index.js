bd236886db41ac7c7121639a68f2b4e0572df3ea
functional-puppet-3692 <samadshaik@live.com>
2018-10-30 20:13:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string){
+  return string.toUpperCase();
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(string){
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
+// function sayHiToGrandma(string){
+//
+//   var lowercase = "hello";
+//   if (string == string.toLowerCase()) {
+//     // return sayHiToGrandma(string);
+//     return string.toLowerCase();
+//   }
+//
+// }
+
+function sayHiToGrandma(string) {
+    if (string.toLowerCase()) {
+      //return string.toLowerCase();
+      console.log(string);
+    }
+
+}
