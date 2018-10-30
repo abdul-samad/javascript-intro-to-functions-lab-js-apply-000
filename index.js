2413edae635ea5d8a8d659a06fd4fb5508a22434
functional-puppet-3692 <samadshaik@live.com>
2018-10-30 21:27:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,8 @@ function logWhisper(string){
 
 function sayHiToGrandma(string) {
 
-  if (string.toLowerCase()) {
+  if (string === string.toLowerCase()) {
     return "+I can\'t hear you!"
-    //return string.toLowerCase();
   }
 }
 
